310203caf5d468f0ed55d653ae581e53a88ff9ca
Chaloemchai Ongcharun <chaloemchai.ongcharun@gmail.com>
2017-02-07 20:50:53 +0700

    Update bot-add.php

--- bot-add.php
+++ bot-add.php
@@ -11,43 +11,61 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message') {
-      if ($event['message']['type'] == 'text') {
-        // Get text sent
-        $text = $event['message']['text'];
-        // Get replyToken
-        $replyToken = $event['replyToken'];
+			if ($event['message']['type'] == 'text') {
+			// Get text sent
+				$text = $event['message']['text'];
+				// Get replyToken
+				$replyToken = $event['replyToken'];
 
-        // Build message to reply back
-        $messages = [
-          'type' => 'text',
-          'text' => "Return message : ".$text
-        ];
-      } elseif ($event['message']['type'] == 'sticker') {
-        $sticker = $event['message']['sticker'];
-        // Get replyToken
-        $replyToken = $event['replyToken'];
-// ‘type’ => ‘sticker’,
-// ‘packageId’ => ‘4’,
- //‘stickerId’ => ‘300’
-        // Build message to reply back
-        $messages = [
-          'type' => 'sticker',
-          'packageId' => '2',
-          'stickerId' => '525'
-        ];     
-        } elseif ($event['message']['type'] == 'follow') {
-        $sticker = $event['message']['sticker'];
-        // Get replyToken
-        $replyToken = $event['replyToken'];
-
-        // Build message to reply back
-        $messages = [
-          'type' => 'text',
-          'text' => "Thank you for added"
-        ];
-        }
+				// Build message to reply back
+				$messages = [
+				  'type' => 'text',
+				  'text' => "Return message : ".$text
+				// ];
+			} elseif ($event['message']['type'] == 'sticker') {
+				$sticker = $event['message']['sticker'];
+				// Get replyToken
+				$replyToken = $event['replyToken'];
+				// ‘type’ => ‘sticker’,
+				// ‘packageId’ => ‘4’,
+				//‘stickerId’ => ‘300’
+				// Build message to reply back
+				$messages = [
+				  'type' => 'sticker',
+				  'packageId' => '2',
+				  'stickerId' => '525'
+				];     
+			} 
 			
 
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+      
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}elseif ($event['type'] == 'follow') {
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+			  'type' => 'text',
+			  'text' => "Thank you for added"
+			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
